21d3b6b6a9df606ed37f94561d4e81254876ca84
WaiJade <113276957+CheongSzesuen@users.noreply.github.com>
2025-06-08 21:19:30 +0800

    用网络的访问

--- index.js
+++ index.js
@@ -599,7 +599,7 @@ function hideAbout() {
 
 // 加载JSON数据并初始化游戏
 function loadGameData() {
-    fetch('data/events.json')
+    fetch('https://raw.githubusercontent.com/CheongSzesuen/OK-School-Life-Web/refs/heads/main/data/events.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP错误! 状态: ${response.status}`);
