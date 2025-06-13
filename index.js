92ca32228aeb91d08bb9f0125021418544be21ed
WaiJade <113276957+CheongSzesuen@users.noreply.github.com>
2025-06-13 20:58:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // 游戏数据 URL（用于加载 JSON 文件）
-const GAME_DATA_URL = 'https://cdn.jsdelivr.net/gh/still-alive-hhz/OK-School-Life@main/assets/data/events.json';
+const GAME_DATA_URL = 'https://cdn.jsdelivr.net/gh/CheongSzesuen/OK-School-Life-Web/data/events.json?t=' + Date.now();
 
 const gameState = {
     currentApi: 'choose_start',
